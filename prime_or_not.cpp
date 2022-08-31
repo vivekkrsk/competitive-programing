d1406541c3d27c39386e6a978971c663227bbeb3
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-08-31 08:20:05 +0530

    commit

---
+++ prime_or_not.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int is_Prime(int n)
+{
+    if (n <= 1)
+    {
+        return 0;
+    }
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number to check number is prime or not\n";
+    cin >> n;
+
+    if (is_Prime(n))
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "false";
+    }
+
+    return 0;
+}
