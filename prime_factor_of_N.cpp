d65d487a660eea26e736732baa034f4badf6dec4
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-09-01 12:14:44 +0530

    commit

---
+++ prime_factor_of_N.cpp
@@ -0,0 +1,22 @@
+// Prime factors of a big number
+//       Given a number N, print all the prime factors and their powers.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void prime_factor(int n)
+{
+    
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number to check number is prime or not\n";
+    cin >> n;
+
+    
+
+    return 0;
+}
